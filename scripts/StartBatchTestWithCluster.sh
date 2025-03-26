f06cc0584c5cc0a3efce9396e2df967a0d20642d
alan.turin <1531255491@qq.com>
2025-03-26 15:00:56 +0800

    add data

--- scripts/StartBatchTestWithCluster.sh
+++ scripts/StartBatchTestWithCluster.sh
@@ -1,7 +1,8 @@
 #! /bin/bash
 temp=$(dirname "$0")
+taskfile=$1
 cd ${temp}/..
 project_dir=`pwd`
 echo "ProjectDir="$project_dir ; cd ${project_dir} 
 mkdir ${project_dir}/_cluster_run
-python ${project_dir}/Runtime/kcg/startup_cluster_tasks.py ${project_dir}/ClusterTaskConfigs/task_config.json
+python ${project_dir}/Runtime/kcg/startup_cluster_tasks.py ${taskfile}
