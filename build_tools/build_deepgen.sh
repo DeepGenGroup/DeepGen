193db27e22962e2a02b4b6e1c1e617621ea34645
alan.turin <1531255491@qq.com>
2025-05-28 14:27:42 +0800

    tempadd

--- build_tools/build_deepgen.sh
+++ build_tools/build_deepgen.sh
@@ -1,12 +1,14 @@
 #! /bin/bash
 project_dir="$HOME/DeepGen"
 cd $project_dir
-is_as_pymodule='ON'
-enable_debug_mode='OFF'
+is_as_pymodule='OFF'
+enable_debug_mode='ON'
 mkdir build
 mkdir _dump
 cd build  
 cmake .. \
+    -DCMAKE_CXX_COMPILER=/usr/bin/g++ \
+    -DCMAKE_C_COMPILER=/usr/bin/gcc \
     -DCOMPILE_AS_PYMODULE=$is_as_pymodule \
     -DENABLE_DEBUG_LOG=$enable_debug_mode \
     -Dpybind11_DIR=$HOME/anaconda3/envs/py310/lib/python3.10/site-packages/pybind11/share/cmake/pybind11 \
