4c7c0365651a1a32e3f916a6c819a42b5ab573a2
alan.turin <1531255491@qq.com>
2025-07-14 15:18:14 +0800

    修改cmakelist

--- build_tools/build_deepgen.sh
+++ build_tools/build_deepgen.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 project_dir="$HOME/DeepGen"
 cd $project_dir
-is_as_pymodule='OFF'
-debug_mode='ON'
+is_as_pymodule='ON'
+buildType=MinSizeRel  # Debug Release MinSizeRel 
 mkdir build
 mkdir _dump
 cd build  
@@ -10,7 +10,7 @@ cmake .. \
     -DCMAKE_CXX_COMPILER=/usr/bin/g++ \
     -DCMAKE_C_COMPILER=/usr/bin/gcc \
     -DCOMPILE_AS_PYMODULE=$is_as_pymodule \
-    -DDEBUG_MODE=$debug_mode \
+    -DCMAKE_BUILD_TYPE=$buildType \
     -Dpybind11_DIR=$HOME/anaconda3/envs/py310/lib/python3.10/site-packages/pybind11/share/cmake/pybind11 \
     -Dnanobind_DIR=/home/xushilong/anaconda3/envs/triton_rocm/lib/python3.8/site-packages/nanobind/cmake
 make -j16
